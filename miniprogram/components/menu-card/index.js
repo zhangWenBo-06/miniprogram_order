28eb0cc4fe8584ae7c00c7f22e192ac65e354e36
张文波 <n9jrsu3gh1@rifeng.com>
2026-07-08 11:20:47 +0800

    feat: show brand tag on menu cards

--- miniprogram/components/menu-card/index.js
+++ miniprogram/components/menu-card/index.js
@@ -1,6 +1,17 @@
+const { getSubcategoryLabel } = require('../../utils/categories')
+
 Component({
   properties: {
     menu: { type: Object, value: {} }
+  },
+  data: {
+    brandLabel: ''
+  },
+  observers: {
+    'menu.category, menu.subcategory': function(cat, sub) {
+      const label = sub ? getSubcategoryLabel(cat, sub) : ''
+      this.setData({ brandLabel: label })
+    }
   },
   methods: {
     onTap() {
